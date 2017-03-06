675364a41bbf47e6f58e15ff6b2fd293c592751e
Wei Zhou <867571123@qq.com>
2017-03-06 11:07:35 +0800

    Add items file

---
+++ src/pages/items/items.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+
+import {IService} from '../../services/IService';
+
+@Component({
+  templateUrl: 'items.html',
+  providers: []
+})
+
+export class ItemsPage {
+  title: string;
+  componentName: string;
+  pages: any;
+  listServices : { [key:string]:IService; } = {};
+  service: IService;
+
+  // services: array
+  constructor(
+    public navCtrl: NavController,
+    navParams: NavParams
+  ) {
+
+    this.listServices = {};
+
+    this.componentName = navParams.get('componentName');
+    this.service = this.listServices[this.componentName];
+
+    if (this.service) {
+      this.pages = this.service.getAllThemes();
+      this.title = this.service.getTitle();
+    }
+  }
+
+  selectPageForOpen(value:string):any {
+
+  }
+
+  openPage(page:any) {
+
+  }
+}
