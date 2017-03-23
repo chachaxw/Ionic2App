31b57f4c1168c03e2134e563ecc2cb92a7e42a9c
Wei Zhou <867571123@qq.com>
2017-03-23 14:53:25 +0800

    Add drag and drop

---
+++ src/components/list-view/drag-and-drop/layout-2/drag-and-drop-layout-2.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { reorderArray } from 'ionic-angular';
+
+@Component({
+    selector: 'drag-and-drop-layout-2',
+    templateUrl: 'drag-and-drop.html'
+})
+
+export class DragAndDropLayout2 {
+    @Input() data: any;
+    @Input() events: any;
+
+    constructor() { }
+
+    onEvent(event: string, item: any, e: any) {
+        if (e) {
+            e.stopPropagation();
+        }
+        if (this.events[event]) {
+            this.events[event](item);
+        }
+    }
+
+    reorderItems = (indexes): void => {
+        this.data.items = reorderArray(this.data.items, indexes);
+    }
+}
