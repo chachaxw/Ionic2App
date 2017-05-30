3675e52ffddfd06e65f4563d828e3b7842f13d8a
chacha <867571123@qq.com>
2017-05-30 21:28:21 +0800

    Add tab pages

---
+++ src/pages/tab-page-2/tab-page-2.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  templateUrl: 'tab-page-2.html'
+})
+export class TabPage2 {
+  constructor() {
+
+  }
+}
---
+++ src/pages/tab-page-3/tab-page-3.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  templateUrl: 'tab-page-3.html'
+})
+export class TabPage3 {
+  constructor() {
+
+  }
+}
