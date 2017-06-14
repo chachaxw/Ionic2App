3a921030659d5941161a9bae64f92e534f346578
chacha <867571123@qq.com>
2017-06-14 17:31:39 +0800

    Add epics

--- src/store/epics.ts
+++ src/store/epics.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import { ANIMAL_TYPES } from '../pages/redux-example/model';
+import { AnimalAPIEpics } from '../pages/redux-example/api/epics';
 
 @Injectable()
 export class RootEpics {
-  constructor() {}
+  constructor(private animalAPIEpics: AnimalAPIEpics) {}
 
   public createEpics() {
-    return [];
+    return [
+      this.animalAPIEpics.createEpic(ANIMAL_TYPES.ELEPHANT),
+      this.animalAPIEpics.createEpic(ANIMAL_TYPES.LION),
+    ];
   }
 }
